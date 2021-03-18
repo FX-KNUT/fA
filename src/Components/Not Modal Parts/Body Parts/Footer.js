fba4ba09f5ba5900add1b360a88d67b69b91f155
leejong <znlspf123@naver.com>
2021-03-18 15:20:49 +0900

    2021 03 18 final

--- src/Components/Not Modal Parts/Body Parts/Footer.js
+++ src/Components/Not Modal Parts/Body Parts/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
         <footer id="footer_container" className="h-footer">
-            <div id="footer" className="">&copy; Last Updated: Mar 10 2021, f(x)</div>  
+            <div id="footer" className="">&copy; Last Updated: Mar 18 2021, f(x)</div>  
         </footer>
     );
 };
